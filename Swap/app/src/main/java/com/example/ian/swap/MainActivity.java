9106a2f5cfb40ae59c2693e670bac0965f07e95d
iannech <ianoniango94@gmail.com>
2016-01-14 12:04:44 +0300

    Set up tab fragments

--- Swap/app/src/main/java/com/example/ian/swap/MainActivity.java
+++ Swap/app/src/main/java/com/example/ian/swap/MainActivity.java
@@ -3,20 +3,37 @@ package com.example.ian.swap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        setUpViewPager(viewPager);
+
+        tabLayout =(TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,8 +58,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Set up Account Header
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Root Kontrol")
+                                .withEmail("rootkontrol@gmail.com").withIcon(ResourcesCompat.getDrawable(getResources(),R.drawable.profile, null)))
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                                                 @Override
+                                                 public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                                                     return false;
+                                                 }
+                                             }
+
+                )
+                .build();
+
         //Set up Navigation Drawer
-        new DrawerBuilder().withActivity(this).build();
+        new DrawerBuilder()
+                .withAccountHeader(headerResult)
+                .withActivity(this).build();
 
         PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName(R.string.drawer_item_home);
         SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName(R.string.drawer_item_settings);
@@ -50,15 +93,54 @@ public class MainActivity extends AppCompatActivity {
                         item2,
                         new SecondaryDrawerItem().withName(R.string.drawer_item_settings)
                 )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener(){
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem){
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                         return false;
                     }
                 })
                 .build();
 
 
+    }
+
+    private void setUpViewPager(ViewPager viewPager){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TasksFragment(), "Tasks");
+        adapter.addFragment(new InventoryFragment(),"Inventory");
+        adapter.addFragment(new CompletedFragment(),"Completed");
+        viewPager.setAdapter(adapter);
+
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter( FragmentManager manager){
+            super(manager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return mFragmentTitleList.get(position);
+        }
     }
 
     @Override
